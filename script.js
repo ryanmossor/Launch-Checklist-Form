c84cd7a28c203acdd750da95bd217695b41e40e5
Ryan <ryan.mossor@gmail.com>
2021-03-23 10:33:50 -0500

    Added initial form validation

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// Write your JavaScript code here!
-
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
 <ol>
@@ -11,3 +9,52 @@
 </ol>
 <img src="${}">
 */
+
+function validateName(name) {
+   if (/^[a-z. ]+$/gi.test(name)) {
+      return true;
+   } else {
+      return false;
+   }
+}
+
+const form = document.getElementById("launchForm");
+
+function validate(event) {
+// form.addEventListener("submit", (event) => {
+   // event.preventDefault();
+   const pilotName = document.getElementById("pilotName").value.trim();
+   const copilotName = document.getElementById("copilotName").value.trim();
+   const fuelLevel = document.getElementById("fuelLevel").value.trim();
+   const cargoMass = document.getElementById("cargoMass").value.trim();
+   let validStatus = true;
+
+   if (pilotName == null || !validateName(pilotName)) {
+      validStatus = false;
+   } else if (copilotName == null || !validateName(copilotName)) {
+      validStatus = false;
+   } else if (fuelLevel === '' || fuelLevel == null || isNaN(fuelLevel)) {
+      validStatus = false;
+   } else if (cargoMass === '' || cargoMass == null || isNaN(cargoMass)) {
+      validStatus = false;
+   }
+
+   console.log(pilotName);
+   console.log(copilotName);
+   console.log(fuelLevel);
+   console.log(cargoMass);
+   console.log(validStatus);
+
+   if (validStatus === false) {
+      alert("All fields are required!");
+      event.preventDefault();
+   } else {
+      event.preventDefault();
+   }
+// });
+}
+
+window.addEventListener("load", function() {
+   let newForm = document.getElementById("launchForm");
+   newForm.addEventListener("submit", validate);
+});
