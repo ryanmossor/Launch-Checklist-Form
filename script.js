7ab189a193a10835a146f2fa81fa89dc6515a3f4
Ryan <ryan.mossor@gmail.com>
2021-03-25 08:42:56 -0500

    Added fetchData and updateDestination functions. Cleaned up validation function

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-/* This block of code shows how to format the HTML once you fetch some planetary JSON!
-<h2>Mission Destination</h2>
-<ol>
-   <li>Name: ${}</li>
-   <li>Diameter: ${}</li>
-   <li>Star: ${}</li>
-   <li>Distance from Earth: ${}</li>
-   <li>Number of Moons: ${}</li>
-</ol>
-<img src="${}">
-*/
-
-const form = document.getElementById("launchForm");
-
 const pilotName = document.getElementById("pilotName");
 const copilotName = document.getElementById("copilotName");
 const fuelLevel = document.getElementById("fuelLevel");
@@ -44,39 +30,68 @@ const updateShuttleRequirements = () => {
       fuelStatus.innerHTML = 'Fuel level high enough for launch';
       cargoStatus.innerHTML = 'Cargo mass low enough for launch';
       launchStatus.innerHTML = 'Shuttle is ready for launch';
+      faultyItems.style.visibility = 'visible';
       launchStatus.style.color = 'green';
    }
 }
 
-const validateForm = event => {
-   let inputs = document.getElementsByTagName("input");
-   let completedStatus = true;
-   let validStatus = true;
+const fetchData = async() => {
+   let response = await fetch('https://handlers.education.launchcode.org/static/planets.json');
+   let data = await response.json();
+   return data;
+}
 
+const updateDestination = async() => {
+   let planetData = await fetchData();
+   let randomIndex = Math.floor(Math.random() * 7)
+   const missionTarget = document.getElementById('missionTarget');
+   
+   missionTarget.innerHTML = `<h2>Mission Destination</h2>
+   <ol>
+      <li>Name: ${planetData[randomIndex].name}</li>
+      <li>Diameter: ${planetData[randomIndex].diameter}</li>
+      <li>Star: ${planetData[randomIndex].star}</li>
+      <li>Distance from Earth: ${planetData[randomIndex].distance}</li>
+      <li>Number of Moons: ${planetData[randomIndex].moons}</li>
+   </ol>
+   <img src="${planetData[randomIndex].image}">`
+   
+}
+
+const isFormComplete = () => {
+   let inputs = document.getElementsByTagName("input");
+   
    // Check that inputs are not null/undefined/empty
    for (let i = 0; i < inputs.length; i++) {
       if (inputs[i].value.trim() == null || inputs[i].value.trim() === '') {
-         completedStatus = false;
+         return false;
       }
    }
+   return true;
+}
 
+const validateInputs = () => {
    // Check that pilot names are valid strings; check that fuel level/cargo mass are numbers
    if (!isValidName(pilotName.value.trim()) || !isValidName(copilotName.value.trim())) {
-      validStatus = false;
+      return false;
    } else if (isNaN(fuelLevel.value.trim()) || isNaN(cargoMass.value.trim())) {
-      validStatus = false;
-   }
-
-   if (completedStatus === false) {
-      alert("All fields are required!");
-      event.preventDefault();
-   } else if (validStatus === false) {
-      alert("Make sure to enter valid information for each field!");
-      event.preventDefault();
+      return false;
    } else {
-      updateShuttleRequirements();
-      event.preventDefault();
+      return true;
    }
 }
 
+const validateForm = event => {
+   event.preventDefault();
+   if (!isFormComplete()) {
+      alert("All fields are required!");
+   } else if (!validateInputs()) {
+      alert("Make sure to enter valid information for each field!");
+   } else {
+      updateShuttleRequirements();
+      updateDestination();
+   }
+}
+
+const form = document.getElementById("launchForm");
 form.addEventListener("submit", validateForm);
